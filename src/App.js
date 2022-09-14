a53e9799ffcd77f7eb36b575008cbabeaa25bc75
Kierra Dahlin <kierra@choice.marketing>
2022-09-14 16:29:28 -0600

    Added some styling

--- src/App.js
+++ src/App.js
@@ -5,12 +5,20 @@ function App() {
   return (
     <div className="App">
       <div className="container main-content">
-        <header className="App-header"></header>
+        <header className="App-header">
+          <h1>What's on your mind?</h1>
+          <p>Search the word you're thinkin of...</p>
+        </header>
         <main>
           <Dictionary />
         </main>
         <footer>
-          <small>Coded by Kierra Dahlin</small>
+          <small>
+            Coded by Kierra Dahlin -{" "}
+            <a href="https://github.com/kierraDahlin/dictionary-project">
+              Github
+            </a>
+          </small>
         </footer>
       </div>
     </div>
